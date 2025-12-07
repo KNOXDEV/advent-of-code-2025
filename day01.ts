3df5fead4e6bc742d81d8ecc53e4c11c057935c7
Nick Knox <nick@knox.codes>
2025-12-07 13:30:02 -0500

    feat: day01 part 2

--- day01.ts
+++ day01.ts
@@ -9,11 +9,37 @@ const rotations = input.trim().split("\n").map((rotation) =>
 
 const results = rotations.reduce((state, curr) => {
     state.rotation += curr;
-    state.rotation %= 100;
+
+    // required for true modulo
+    state.rotation = ((state.rotation % 100) + 100) % 100;
+
     if (state.rotation == 0) {
         state.count += 1;
     }
     return state;
 }, { count: 0, rotation: 50 });
 
-console.log("the password is", results.count);
+console.log("the (part 1) password is", results.count);
+
+// part 2
+const results2 = rotations.reduce((state, curr) => {
+
+    // the case where you started at zero and went backwards
+    if (state.rotation == 0 && curr < 0) {
+        state.count -= 1;
+    }
+
+    state.rotation += curr;
+    state.count += Math.floor(Math.abs(state.rotation) / 100);
+
+    // the negative case
+    if (state.rotation <= 0) {
+        state.count += 1;
+    }
+
+    // required for true modulo
+    state.rotation = ((state.rotation % 100) + 100) % 100;
+    return state;
+}, { count: 0, rotation: 50 });
+
+console.log("the (part 2) password is", results2.count);
